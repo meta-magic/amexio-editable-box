6300858eba4211aaf0f49f26a1361cdae78625f6
ketangote <ketan.gote@metamagic.in>
2020-09-04 22:03:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,75 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amexio-editable-box';
+  data = [];
+
+  constructor(){
+    this.data = [
+      {
+        "type": "label",
+        "content": "Reason for contact:",
+        "color": "black",
+        "newline": false
+      },
+      {
+        "type": "label",
+        "content": "Unable to view driver file in DM",
+        "color": "black",
+        "newline": true
+      },
+      {
+        "type": "label",
+        "content": "Internal action taken:",
+        "color": "black",
+        "newline": false
+      },
+      {
+        "type": "label",
+        "content": "Changed from",
+        "color": "black",
+        "newline": false
+      },
+      {
+        "type": "editabletextfield",
+        "content": "ABILENE, TX-PEPSI BEV CO.",
+        "color": "purple",
+        "newline": true
+      },
+      {
+        "type": "label",
+        "content": "to",
+        "color": "black",
+        "newline": false
+      },
+      {
+        "type": "editabletextfield",
+        "content": "ABILENE TX, TRANS-PEPSI BEV CO.",
+        "color": "purple",
+        "newline": true
+      },
+      {
+        "type": "label",
+        "content": "Solution provided: Updated driver file to",
+        "color": "black",
+        "newline": false
+      },
+      {
+        "type": "editabletextfield",
+        "content": "ABILENE TX, TRANS-PEPSI BEV CO.",
+        "color": "purple",
+        "newline": false
+      }
+    ];
+    /*
+    this.data.push({'type':'label','content':'Changed From','color':'black','newline':false});
+    this.data.push({'type':'editabletextfield','content':'Mumbai','color':'purple','newline':false});
+    this.data.push({'type':'label','content':'To','color':'black','newline':false});
+    this.data.push({'type':'editabletextfield','content':'New Delhi','color':'purple','newline':true});
+
+    this.data.push({'type':'label','content':'Changed From','color':'black','newline':false});
+    this.data.push({'type':'editabletextfield','content':'Mumbai','color':'purple','newline':false});
+    this.data.push({'type':'label','content':'To','color':'black','newline':false});
+    this.data.push({'type':'editabletextfield','content':'New Delhi','color':'purple','newline':true});
+    */
+  }
 }
---
+++ src/app/component/amexio-editable-box.component.ts
@@ -0,0 +1,24 @@
+import { OnInit, Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'amexio-editable-box',
+    templateUrl: './amexio-editable-box.component.html',
+    styleUrls: ['./amexio-editable-box.component.css']
+})
+export class AmexioEditableBoxComponent implements OnInit {
+
+    @Input() data: any[];
+
+    constructor() {
+        this.data = [];
+    }
+
+    ngOnInit() {
+
+    }
+
+    updateData(event: any, item:any){
+        item.content = event.target.innerHTML;
+    }
+
+}
